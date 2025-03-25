074f585ad04bb4734507df6c4e0eab6dec32ba24
Levisantos <levi.santos@melhorenvio.com>
2025-03-24 22:27:55 -0300

    update

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -6,14 +6,81 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
   })
 
-  it.only('preenche os campos obrigatórios e envia o formulário', () => {
+  it('preenche os campos obrigatórios e envia o formulário', () => {
     const longText = Cypress._.repeat('12345679',10)
     cy.get('#firstName').should('be.visible').type('Levi')
     cy.get('#lastName').should('be.visible').type('Carvalho')
     cy.get('#email').should('be.visible').type('levysantos792@gmail.com')
     cy.get('#phone').should('be.visible').type('79999999999')
     cy.get('#open-text-area').should('be.visible').type(longText, {delay: 0})
-    cy.get('.button, Enviar').should('be.visible').click()
+    cy.contains('button', 'Enviar').click()
     cy.get('.success').should('be.visible')
   })
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.FillMandatoryFieldsAndSubmit()
+    cy.get('.success').should('be.visible')
+  })
+
+  it('preenche os campos obrigatórios e envia o formulário com diferentes dados', () => {
+    const  teste = {
+      firstName: 'Levi',
+      lastName: 'Carvalho',
+      email: 'Levi@gmail.com',
+      phone: '79999999999'
+    }
+    cy.FillMandatoryFieldsAndSubmit(teste)
+    cy.get('.success').should('be.visible')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').should('be.visible').type('Levi')
+    cy.get('#lastName').should('be.visible').type('Carvalho')
+    cy.get('#email').should('be.visible').type('levysantos792@gmail,com')
+    cy.contains('button', 'Enviar').click()
+    cy.get('.error').should('be.visible')
+
+  })
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').should('be.visible').type('Levi')
+    cy.get('#lastName').should('be.visible').type('Carvalho')
+    cy.get('#email').should('be.visible').type('levysantos792@gmail.com')
+    cy.get('#phone-checkbox').should('be.visible').click()
+    // cy.get('#phone').should('be.visible').type('').should('have.value', '')
+    cy.get('#email').should('be.visible').type('levysantos792@gmail,com')
+    cy.contains('button', 'Enviar').click()
+    cy.get('.error').should('be.visible')
+  })
+
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName').should('be.visible').type('Levi').should('not.be.value', '').clear().should('be.value', '')
+    cy.get('#lastName').should('be.visible').type('Carvalho').should('not.be.value', '').clear().should('be.value', '')
+    cy.get('#email').should('be.visible').type('levysantos792@gmail.com').should('not.be.value', '').clear().should('be.value', '')
+    cy.get('#phone').should('be.visible').type('79999999999').should('have.value', '79999999999').should('not.be.value', '').clear().should('be.value', '')
+    cy.contains('button', 'Enviar').click()
+    cy.get('.error').should('be.visible')
+
+  })
+
+  //usando select
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product').select('youtube').should('have.value', 'youtube') //texto
+  })
+  it('seleciona um produto (Mentoria) por seu texto', () => {
+    cy.get('#product').select('mentoria').should('have.value', 'mentoria') //value
+  })
+  it('seleciona um produto (Blog) por seu texto', () => {
+    cy.get('#product').select(1).should('have.value', 'blog') //indice
+  })
+
+  it('marca o tipo de atendimento Feedback', () => {
+    cy.get('input[type="radio"][value="feedback"]').should('be.visible').check().should('be.checked')
+
+  })
+  it.only('marca o tipo de atendimento', () => {
+    cy.get('input[type="radio"]').each(($radio) => {
+      cy.wrap($radio).check().should('be.checked')
+    })
+  })
+  
+
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,27 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('FillMandatoryFieldsAndSubmit', () => {
+    const longText = Cypress._.repeat('12345679',10)
+    const firstName = 'Levi'
+    const lastName = 'Carvalho'
+    const phone = '79999999999'
+    const email = 'levysantos792@gmail.com'
+    cy.get('#firstName').should('be.visible').type(firstName)
+    cy.get('#lastName').should('be.visible').type(lastName)
+    cy.get('#email').should('be.visible').type(email)
+    cy.get('#phone').should('be.visible').type(phone)
+    cy.get('#open-text-area').should('be.visible').type(longText, {delay: 0})
+    cy.get('.button, Enviar').should('be.visible').click()
+})
+Cypress.Commands.add('FillMandatoryFieldsAndSubmit', (data = {
+    firstName: 'Mariazinha',
+    lastName: 'pereirinha',
+    email: 'mariazinha@gmail.com',
+    phone: '79999999999'
+}) => {
+    const longText = Cypress._.repeat('12345679',10)
+    cy.get('#firstName').should('be.visible').type(data.firstName)
+    cy.get('#lastName').should('be.visible').type(data.lastName)
+    cy.get('#email').should('be.visible').type(data.email)
+    cy.get('#phone').should('be.visible').type(data.phone)
+    cy.get('#open-text-area').should('be.visible').type(longText, {delay: 0})
+    cy.get('.button, Enviar').should('be.visible').click()
+})
